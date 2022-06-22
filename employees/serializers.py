27f030ac3367f75ac7346c42739756d5b16ae70b
Nurgali Nuraly <nuralynurgalix@gmail.com>
2022-06-22 13:47:34 +0600

    2

--- employees/serializers.py
+++ employees/serializers.py
@@ -3,15 +3,7 @@ from rest_framework import serializers
 from .models import *
 
 
-class CategorySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Category
-        fields = ('positions',)
-
-
 class EmployeesSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(many=True, read_only=True)
 
     class Meta:
         model = Employees
